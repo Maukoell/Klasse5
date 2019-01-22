25b94fba7aa3b067b602b03afb934b2ddecde50f
Maukoell <27849117+Maukoell@users.noreply.github.com>
2019-01-23 00:27:40 +0100

    Update

--- SmartMirror/MMM-MusicPlayer/MMM-MusicPlayer.js
+++ SmartMirror/MMM-MusicPlayer/MMM-MusicPlayer.js
@@ -1,18 +1,12 @@
-Module.register('MMM-MusicPlayer',{	
+Module.register("MMM-MusicPlayer",{	
 	defaults: {
 		playButtonPIN: 10,
         	nextButtonPIN: 12,
         	clickDelay: 500,
-        	musicPath,		
-	},	
-	// Override socket notification handler.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === 'BUTTON_PRESSED'){
-			Log.info('Button Test');
-		}
+        	musicPath = "/home/pi/MagicMirror/modules/MMM-MusicPlayer/music",		
 	},	
 	start: function() {
-		this.sendSocketNotification('BUTTON_CONFIG', this.config);
+		this.sendSocketNotification("BUTTON_CONFIG", this.config);
 		Log.info('Starting module: ' + this.name);
 	}
 });
--- SmartMirror/MMM-MusicPlayer/node_helper.js
+++ SmartMirror/MMM-MusicPlayer/node_helper.js
@@ -1,79 +1,89 @@
-const NodeHelper = require('node_helper');
-const gpio = require('onoff');
+"use strict";
+
+const NodeHelper = require("node_helper");
+const gpio = require("onoff");
 var path = require('path');
 var fs = require('fs');
 const { Howl, Howler } = require('howler');
-
+var sound;
+var musicList = [];
+var index = 0;
+var started;
+var config;
 module.exports = NodeHelper.create({
     start: function () {
-        this.started = false
+        started = false;
     },
     // Subclass socketNotificationReceived received.
     socketNotificationReceived: function (notification, payload) {
         const self = this;
-        if (notification === 'BUTTON_CONFIG' && this.started == false) {
-            const self = this
-            this.config = payload
-            fromDir(this.config.musicPath, "mp3");
-            createSound();
-            var GPIO = require('onoff').Gpio,
-                button1 = new GPIO(this.config.playButtonPIN, 'in', 'both', { persistentWatch: true, debounceTimeout: this.config.clickDelay });
+        if (notification === 'BUTTON_CONFIG' && started === false) {
+            config = payload;
+            self.fromDir(config.musicPath, "mp3");
+            self.createSound();
+            var GPIO = require('onoff').Gpio;
+            var button1 = new GPIO(config.playButtonPIN, 'in', 'both', { persistentWatch: true, debounceTimeout: config.clickDelay });
             button1.watch(function (err, state) {
                 // 1 == pressed, 0 == not pressed
-                if (state == 1) {
+                if (state === 1) {
 
                     if (sound.playing()) {
-                        this.sound.pause();
+                        sound.pause();
                     } else {
-                        this.sound.play();
+                        sound.play();
                     }
                 }
             });
-            button2 = new GPIO(this.config.playButtonPIN, 'in', 'both', { persistentWatch: true, debounceTimeout: this.config.clickDelay });
+            var button2 = new GPIO(config.playButtonPIN, 'in', 'both', { persistentWatch: true, debounceTimeout: config.clickDelay });
             button2.watch(function (err, state) {
                 // 1 == pressed, 0 == not pressed
-                if (state == 1) {
-                    playNext();
+                if (state === 1) {
+                    self.playNext();
                 }
-            }),
-                this.started = true;
-        };
+            });
+                started = true;
+        }
     },
+
+    fromDir: function(startPath, filter) {
+        if (!fs.existsSync(startPath)) {
+            Log.info("no dir ", startPath);
+            return;
+        }
+    
+        var files = fs.readdirSync(startPath);
+        var i;
+        var fullPath;
+        var filename;
+        var stat;
+        for (i = 0; i < files.length; i+=1) {
+            fullPath = path.join(startPath, files[i]);
+            filename = files[i];
+            stat = fs.lstatSync(fullPath);
+            if (stat.isFile()) {
+                if (filename.indexOf(filter) >= 0) {
+                    musicList.push(fullPath);
+                }
+            }
+        }
+    },
+
+    playNext: function() {
+        if (sound.playing()) {
+            sound.stop();
+        }
+        index += 1;
+        if (index === musicList.length()) {
+            index = 0;
+        }
+        sound.play(index);
+    },
+
+    createSound: function() {
+        sound = new Howl({
+            src: musicList
+        });
+    }
+
 }
 );
-
-function playNext() {
-    if (this.sound.playing()) {
-        this.sound.stop();
-    }
-    this.index += 1;
-    if (this.index == this.musicList.length()) {
-        this.index = 0;
-    }
-    this.sound.play(index);
-}
-
-function fromDir(startPath, filter) {
-    if (!fs.existsSync(startPath)) {
-        Log.info("no dir ", startPath);
-        return;
-    }
-
-    var files = fs.readdirSync(startPath);
-    for (var i = 0; i < files.length; i++) {
-        var fullPath = path.join(startPath, files[i]);
-        var filename = files[i];
-        var stat = fs.lstatSync(fullPath);
-        if (stat.isFile()) {
-            if (filename.indexOf(filter) >= 0) {
-                this.musicList.push(fullPath);
-            };
-        };
-    };
-}
-
-function createSound() {
-    this.sound = new Howl({
-        src: this.musicList
-    });
-}
